ba284cfcca8afbc8189809e8a038fa970ead3b2d
Ben Cole <bencole12345@gmail.com>
2018-07-14 11:54:46 +0100

    Create password hashing logic

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'),
-      Schema = mongoose.Schema;
+      Schema = mongoose.Schema,
+      bcrypt = require('bcrypt-nodejs');
 
 const UserSchema = new Schema({
     _id: Schema.Types.ObjectId,
@@ -12,5 +13,26 @@ const UserSchema = new Schema({
         required: true
     }
 });
+
+UserSchema.pre('save', function(next) {
+    const user = this,
+          SALT_FACTOR = 8;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+};
 
 module.exports = mongoose.model('User', UserSchema);
