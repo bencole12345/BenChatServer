dfdb0998ce4dae46be967392dd32e847534df803
Ben Cole <bencole12345@gmail.com>
2018-07-14 16:00:41 +0100

    Setup skeleton routes for /conversations

---
+++ controllers/chat.js
@@ -0,0 +1,14 @@
+const Conversation = require('../models/conversation');
+const Message = require('../models/message');
+
+exports.createConversation = function(req, res) {
+
+};
+
+exports.sendMessage = function(req, res) {
+
+};
+
+exports.getMessages = function(req, res) {
+
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 
 const AuthenticationController = require('../controllers/authentication');
+const ChatController = require('../controllers/chat');
 
 const passportService = require('../initialisation/passport');  // Setup, registers the LocalStrategy
 const requireLogin = passport.authenticate('local', { session: false });  // Middleware
@@ -23,7 +24,10 @@ router.use('/auth', authRouter);
 authRouter.use('/login', requireLogin, AuthenticationController.login);
 authRouter.use('/register', AuthenticationController.register);
 
-
-
+const conversationRouter = express.Router();
+router.use('/conversations', conversationRouter);
+conversationRouter.post('/', requireLogin, ChatController.createConversation);
+conversationRouter.get('/:conversationId', requireLogin, ChatController.getMessages);
+conversationRouter.post('/:conversationId', requireLogin, ChatController.sendMessage);
 
 module.exports = router;
